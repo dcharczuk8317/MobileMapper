88354d7ba61d8f0c760bb45423d9e4e45c9a95f3
student5 <student3@jh-mba158-05.local>
2017-03-21 10:02:37 -0500

    Added point for Mt. Rushmore

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -12,16 +12,29 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    let herseyAnnotation = MKPointAnnotation()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let herseyAnnotation = MKPointAnnotation()
-        let latitude: Double = 42.102332924
+
+        let latitude: Double = 42.102337924
         let longitude: Double = -87.955667844
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         herseyAnnotation.coordinate = coordinate
         herseyAnnotation.title = "John Hersey High School"
         mapView.addAnnotation(herseyAnnotation)
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for place in placemarks!
+            {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
 
 }
